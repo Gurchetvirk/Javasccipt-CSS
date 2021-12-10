01aa0b3787e84fe64c4c119ee45771997fd0425b
Gurchetvirk <gurchets2599@gmail.com>
2021-12-10 15:04:31 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+var listItems = ["Mango","Banana","Apples","Grapes","PineApples"]
+var ul = document.querySelector('.container > .list > ul');
+var listItem = ul.children;
+for(var i=0;i<listItem.length;i++)
+{
+    listItem[i].innerHTML=listItems[i];
+}
+console.log(listItem);
+var li = document.querySelector('.container > .list > ul > li:nth-child(3)');
+var textField = document.querySelector('input[type=text]');
+let count = 0;
+li.addEventListener("mouseover",function(){
+    if(count <= 0)
+    {
+        var div= document.createElement("div");
+        var text = document.createTextNode("3rd Item Selected");
+        div.append(text);
+        li.append(div);
+        count++;
+    }
+});
+
+li.addEventListener("mouseout",function(){
+    if(count > 0)
+    {
+        var newElement = document.querySelector('.container > .list > ul > li:nth-child(3) > div');
+        newElement.remove();
+        count--;
+    }
+});
+
+textField.addEventListener('keydown',function()
+    {
+        var writingDiv = document.querySelector('.container > .input > .writing');
+        writingDiv.style.display="block";
+});
+textField.addEventListener('keyup',function()
+    {
+        var writingDiv = document.querySelector('.container > .input > .writing');
+        writingDiv.style.display="none";
+});
+
